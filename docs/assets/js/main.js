bfe73844735cc0902d1f1c3e620428e1e1f9f2d2
pgno <pierre@pop.eu.com>
2018-03-20 15:11:19 +0100

    initial commit

---
+++ docs/assets/js/main.js
@@ -0,0 +1,70 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+var swatch = ['#4D5061', '#EDD2E0', '#EDBBB4', '#DBABBE', '#BAA1A7','#4D5061', '#EDD2E0', '#EDBBB4', '#DBABBE', '#BAA1A7'];
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+function getColorFromPalette(index){
+	return swatch[index];
+}
+
+
+
+var slider;
+var slideCount = [];
+//var url = 'https://raw.githubusercontent.com/le-poplab/outils-et-methodes/master/oulipop/'+window.location.hash.replace('#', '')+'.csv';
+var url = 'assets/data/'+window.location.hash.replace('#', '')+'.csv';
+console.log(url);
+$(document).ready(function(){
+	$.get(url, { "_": $.now() }, function( data ) {
+	  lists = data.match(/[^\r\n]+/g);
+
+	  var items = [];
+	  var nb_blocks = data.length;
+	  $.each( lists, function( index, list ) {
+	  	
+	  	items.push( "<div class='block'>");
+	  	items.push( "<div class='scroller'>");
+	  	list = list.split(',');
+		slideCount[index] = list.length;		
+	  	$.each( list, function( index, value ) {
+	    	items.push( "<div class='element'><div>" + value + "</div></div>" );
+	    });
+	    items.push( "</div>");
+	    items.push( "</div>");
+	  });
+	 
+	  $( "<div/>", {
+	    "class": "blocks",
+	    html: items.join( "" )
+	  }).appendTo( ".container" );
+
+	  $('.block').css("height",100/nb_blocks+"%");
+	  $('.block').each(function(index){
+	  	$(this).css("background-color",getColorFromPalette(index));
+	  })
+	  
+
+	  slider = $('.scroller').slick({
+		arrows : false
+	  });
+	});
+
+	$('.reload').on('click',function(e){
+		console.log('ok');
+		$('.scroller').each(function(index){
+			console.log(slideCount[index]);
+	  		$(this).slick('slickGoTo',getRandomInt(slideCount[index]));
+	  	})
+	})
+
+});
